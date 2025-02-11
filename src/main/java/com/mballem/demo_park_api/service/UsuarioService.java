6c976b3a8b2b81ba0f3c94c43c76884a882b3696
daniel cooder <danielcooder@gmail.com>
2025-02-11 17:01:37 -0300

    recurso para criacao de novo usuario

--- src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.mballem.demo_park_api.service;
+import com.mballem.demo_park_api.config.entity.Usuario;
 import com.mballem.demo_park_api.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,11 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
 
+        return usuarioRepository.save(usuario);
+
+
+    }
 }
