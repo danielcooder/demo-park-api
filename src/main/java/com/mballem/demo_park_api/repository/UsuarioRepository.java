d5811a906d81164069227dcc526b9e729a10fbce
daniel cooder <danielcooder@gmail.com>
2025-02-11 16:49:59 -0300

    criando usuario repository, usuario service e usuario controller

---
+++ src/main/java/com/mballem/demo_park_api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.mballem.demo_park_api.repository;
+
+import com.mballem.demo_park_api.config.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+}
---
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.mballem.demo_park_api.service;
+import com.mballem.demo_park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+
+}
